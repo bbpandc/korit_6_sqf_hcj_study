5a18c7ae8496ce3a04dd5a133d7dc2dab53bdfe0
bbpandc <haeminsia@gmail.com>
2024-06-18 17:45:23 +0900

    js

--- javascript/todolist.js
+++ javascript/todolist.js
@@ -1,14 +1,5 @@
 let todoList = [];
-
-let emptyTodo = {
-    id: 0,
-    content: "",
-    date: ""
-}
-
-let todo = {
-    ...emptyTodo
-}
+loadTodoList();
 
 function handleTodoInputKeyDown(e) {
     if(e.keyCode === 13) {
@@ -17,7 +8,8 @@ function handleTodoInputKeyDown(e) {
 }
 
 function handleTodoOkClick(e) {
-    if(isBlank(document.querySelector(".todo-input"))) {
+    const todoInput = document.querySelector(".todo-input");
+    if(isBlank(todoInput)) {
         alert("내용을 입력하세요.");
         clearTodoInput();
         return;
@@ -27,15 +19,47 @@ function handleTodoOkClick(e) {
 }
 
 function addTodo() {
-    const now = new Date();
-    
-
     const todo = {
-        id: 0,
+        id: createNewId(),
         content: document.querySelector(".todo-input").value,
         date: transformDate(new Date())
     }
-    console.log(todo);
+
+    todoList = [ ...todoList, todo ];
+    saveLocalStorage();
+    loadTodoList();
+}
+
+function createNewId() {
+    const todoIdList = todoList.map(todo => todo.id);
+    const maxId = !todoIdList.length ? 0 : Math.max.apply(null, todoIdList);
+    return maxId + 1;
+}
+
+function saveLocalStorage() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function loadTodoList() {
+    const lsTodoList = localStorage.getItem("todoList");
+    todoList = !lsTodoList ? [] : JSON.parse(lsTodoList);
+    renderTodoList();
+}
+
+function renderTodoList() {
+    const todoListContainer = document.querySelector(".todo-list-conatiner");
+    todoListContainer.innerHTML = todoList.map(todo => {
+        return `
+            <li class="todo-card">
+                <h3 class="todo-date">${todo.date}</h3>
+                <p class="todo-content">${todo.content}</p>
+                <div class="todo-buttons">
+                    <button class="button edit-button" onclick="handleEditClick(event)" value="${todo.id}">수정</button>
+                    <button class="button delete-button" onclick="handleDeleteClick(event)" value="${todo.id}">삭제</button>
+                </div>
+            </li>
+        `;
+    }).join("");
 }
 
 function clearTodoInput() {
@@ -52,5 +76,62 @@ function isBlank(input) {
 function transformDate(date) {
     const dayList = [ "일", "월", "화", "수", "목", "금", "토" ];
     return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()} (${dayList[date.getDay()]}) ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    // -> 2024.05.18(화)
+    // -> 2024.05.18 (화) 14:56:57 
+}
+
+function handleDeleteClick(e) {
+    // 확인: true, 취소: false
+    if(confirm("정말로 삭제하시겠습니까?")) {
+        todoList = todoList.filter(todo => todo.id !== parseInt(e.target.value));
+        saveLocalStorage();
+        loadTodoList();
+    }
+}
+
+function handleEditClick(e) {
+    const element = `
+        <div class="modal-edit-container" onclick="event.stopPropagation()">
+            <h3 class="modal-title">TODO 수정하기</h3>
+            <div class="input-box">
+                <input type="text" class="todo-input" onkeyDown="if(event.keyCode === 13) document.querySelector('.modal button:nth-of-type(1)').click()">
+            </div>
+            <div class="todo-buttons">
+                <button class="button" onclick="handleEditOkClick(event)" value="${e.target.value}">확인</button>
+                <button class="button" onclick="closeModal()">취소</button>
+            </div>
+        </div>
+    `;
+    openModal(element);
+}
+
+function handleEditOkClick(e) {
+    todoList = todoList.map(todo => {
+        if(todo.id === parseInt(e.target.value)) {
+            return {
+                ...todo,
+                content: document.querySelector(".modal .todo-input").value,
+                date: transformDate(new Date)  
+            }
+        }
+        return todo;
+    })
+    saveLocalStorage();
+    closeModal();
+    loadTodoList();
+}
+
+function handleModalBackgroundClick() {
+    closeModal();
+}
+
+function openModal(element) {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("modal-show");
+    modal.innerHTML = element;
+}
+
+function closeModal() {
+    const modal = document.querySelector(".modal");
+    modal.innerHTML = "";
+    modal.classList.remove("modal-show");
 }
